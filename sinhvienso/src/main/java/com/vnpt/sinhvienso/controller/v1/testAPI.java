28db38243193f3ef59c85cc307956f9196103017
trungnguyenM01 <nguyenB2111995@student.ctu.edu.vn>
2025-06-13 10:37:58 +0700

    Create tree of folder for project

--- sinhvienso/src/main/java/com/vnpt/sinhvienso/controller/v1/testAPI.java
+++ sinhvienso/src/main/java/com/vnpt/sinhvienso/controller/v1/testAPI.java
@@ -1,13 +1,13 @@
-package com.vnpt.sinhvienso.controller;
+package com.vnpt.sinhvienso.controller.v1;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/hello")
+@RequestMapping("/api/v1")
 public class testAPI {
-    @GetMapping("")
+    @GetMapping("/test-hello")
     public String getStarted(){
         return "Hello";
     }
